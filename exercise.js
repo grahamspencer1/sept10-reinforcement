aeadb56ca49aab1900f33c9e009c683ab130956a
Graham Spencer <grahamspence@gmail.com>
2018-09-10 12:15:24 -0400

    exercise complete

---
+++ exercise.js
@@ -0,0 +1,17 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+console.log("Todo:");
+for(var i = 0; i < items.length; i++){
+  console.log((i + 1) + ": " + items[i]);
+}
+
+
+
+
+
+
+
+// Todo:
+// 1: purchase domain
+// 2: install SSL certificate
+// 3: deploy site
+// 4: celebrate success
